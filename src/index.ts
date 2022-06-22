8ea7617010a430402d8bf6a352cc6fb99de7f986
Anastasia Petrykovets <nastyapetryk020202@gmail.com>
2022-06-22 12:20:03 +0300

    Second lab fixed

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,18 @@ import { fastify } from 'fastify'
 import db from './config/db'
 import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload'
 import { join } from 'path'
-import cors from '@fastify/cors'
-import { bootstrap } from 'fastify-decorators'
 const pino = require('pino')
 const Port = process.env.PORT || 3000
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/blogs'
+//const winston = require('winston');
+
+
 
 const server = fastify({
   logger: pino({ level: 'info' })
 })
 
-// register plugin below:
 server.register(db, { uri })
-server.register(cors, {
-  origin: (origin: string, cb: any) => {
-    const allowedOrigins = ['http://localhost:3000']
-    if (allowedOrigins.includes(origin) || !origin) {
-      cb(null, true)
-    } else {
-      cb(new Error('Not allowed by CORS'), false)
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  credentials: true,
-  maxAge: 86400,
-  optionsSuccessStatus: 204
-})
 
 server.register(AutoLoad, {
   dir: join(__dirname, 'plugins')
@@ -48,3 +33,68 @@ const start = async () => {
   }
 }
 start()
+
+
+//var init = (appname: string) => {
+
+//    //// Here we use winston.containers IoC
+//    winston.loggers.add('default', {
+//        level: 'info',
+//        format: winston.format.combine(winston.format.splat(), winston.format.json()),
+//        transports: [
+//            new winston.transports.Console({
+//                level: 'debug',
+//            }),
+//            new winston.transports.File({
+//                filename: 'api.log',
+//                dirname: 'logs/info',
+//                format: winston.format.json(),
+//            }),
+//            new winston.transports.File({
+//                level: 'error',
+//                filename: 'errors.log',
+//                dirname: 'logs/error',
+//                format: winston.format.json(),
+//            }),
+//        ],
+//    });
+
+//    //// Here we use winston.containers IoC get accessor
+//    var logger = winston.loggers.get("default");
+
+//    logger.add(new winston.transports.Console({
+//         format: winston.format.simple(),
+//         handleExceptions: true
+//    }));
+
+
+//    process.on('uncaughtException', function (err) {
+//        console.log("UncaughtException processing: %s", err);
+//    });
+
+//    //// PINO like, we link winston.containers to use only one instance of logger
+//    logger.child = function () { return winston.loggers.get("default") };
+
+//    return logger
+//}
+
+//const server = require('fastify')({
+//    logger: init("mytestapp")
+//})
+
+//// register plugin below:
+//server.register(db, { uri })
+
+//server.register(AutoLoad, {
+//  dir: join(__dirname, 'plugins')
+//})
+
+//server.register(AutoLoad, {
+//  dir: join(__dirname, 'routes')
+//})
+
+//server.listen(Port, '0.0.0.0', (err:any, address:any) => {
+//    if (err) throw err
+//    server.log.info(`API server listening on ${address}`)
+//})
+
